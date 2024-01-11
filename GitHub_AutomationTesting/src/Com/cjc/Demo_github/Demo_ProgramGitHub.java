9ac23e80cb9392ab80ed6538c01ec8b64db3d03d
HP <HP@LAPTOP-KUH9GEJ5>
2024-01-11 11:43:54 +0530

    By Jayshree

--- GitHub_AutomationTesting/src/Com/cjc/Demo_github/Demo_ProgramGitHub.java
+++ GitHub_AutomationTesting/src/Com/cjc/Demo_github/Demo_ProgramGitHub.java
@@ -2,9 +2,36 @@ package Com.cjc.Demo_github;
 
 public class Demo_ProgramGitHub 
 {
+	public void m1()
+	{
+		System.out.println("m1---A");
+	}
+	public void m2()
+	{
+		System.out.println("m2----B");
+	}
+	public void m3()
+	{
+		System.out.println("m3--C");
+	}
+	public void m4()
+	{
+		System.out.println("M4--D");
+	}
+	public void m5()
+	{
+		System.out.println("M5--E");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome in CJC");
+		
+		Demo_ProgramGitHub d=new Demo_ProgramGitHub ();
+		d.m1();
+		d.m2();
+		d.m3();
+		d.m4();
+		d.m5();
 		
 	}
 
